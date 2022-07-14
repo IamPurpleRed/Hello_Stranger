0a81dda92331962a19c70dfd3ff0c082639b0651
PurpleRed <benson060211@gmail.com>
2022-07-15 00:05:47 +0800

    feat: image_cropper 套件實作

--- lib/screens/enroll_page.dart
+++ lib/screens/enroll_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
 
@@ -40,40 +41,7 @@ class _EnrollPageState extends State<EnrollPage> {
             SizedBox(
               width: vw * 0.6,
               height: vw * 0.6,
-              child: Stack(
-                children: [
-                  Positioned.fill(
-                    child: ClipOval(
-                      child: FittedBox(
-                        fit: BoxFit.fill,
-                        child: (accountPhoto == null) ? Image.asset('assets/default_account_photo.png') : Image.file(accountPhoto!),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: vw * 0.45,
-                    bottom: 0,
-                    child: ClipOval(
-                      child: Container(
-                        width: vw * 0.15,
-                        height: vw * 0.15,
-                        color: Palette.secondaryColor,
-                        child: IconButton(
-                          icon: LayoutBuilder(
-                            builder: (context, constraints) => Icon(
-                              Icons.image_search,
-                              size: constraints.maxWidth,
-                            ),
-                          ),
-                          color: Colors.white,
-                          splashRadius: vw * 0.06,
-                          onPressed: pickAndCropImage,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              child: accountPhotoArea(vw),
             ),
           ],
         ),
@@ -81,18 +49,54 @@ class _EnrollPageState extends State<EnrollPage> {
     );
   }
 
-  void pickAndCropImage() async {
+  /* INFO: 使用者頭貼 */
+  Stack accountPhotoArea(double vw) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: ClipOval(
+            child: FittedBox(
+              fit: BoxFit.fill,
+              child: (accountPhoto == null) ? Image.asset('assets/default_account_photo.png') : Image.file(accountPhoto!),
+            ),
+          ),
+        ),
+        Positioned(
+          left: vw * 0.45,
+          bottom: 0,
+          child: ClipOval(
+            child: Container(
+              width: vw * 0.15,
+              height: vw * 0.15,
+              color: Palette.secondaryColor,
+              child: IconButton(
+                icon: LayoutBuilder(
+                  builder: (context, constraints) => Icon(
+                    Icons.image_search,
+                    size: constraints.maxWidth,
+                  ),
+                ),
+                color: Colors.white,
+                splashRadius: vw * 0.06,
+                onPressed: pickAndCropImage,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  /* INFO: 選擇並裁剪照片 */
+  Future<void> pickAndCropImage() async {
+    XFile? pickedImage;
+
     try {
-      final XFile? pickedImage = await widget.picker.pickImage(
+      pickedImage = await widget.picker.pickImage(
         source: ImageSource.gallery,
       );
-
       if (pickedImage == null) {
-        return;
-      } else {
-        setState(() {
-          accountPhoto = File(pickedImage.path);
-        });
+        return; // 使用者沒有或是取消選取照片
       }
     } on PlatformException catch (e) {
       Widgets.dialog(
@@ -100,6 +104,33 @@ class _EnrollPageState extends State<EnrollPage> {
         title: '上傳圖片失敗',
         content: '原因：${e.message ?? '未知的錯誤'}',
       );
+      return;
+    }
+
+    CroppedFile? croppedFile = await ImageCropper().cropImage(
+      sourcePath: pickedImage.path,
+      aspectRatioPresets: [CropAspectRatioPreset.square],
+      maxWidth: 500,
+      maxHeight: 500,
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarTitle: '裁剪您的帳戶圖片',
+          toolbarColor: Palette.primaryColor,
+          toolbarWidgetColor: Colors.white,
+          activeControlsWidgetColor: Palette.secondaryColor,
+          initAspectRatio: CropAspectRatioPreset.original,
+        ),
+        IOSUiSettings(
+          title: '裁剪您的帳戶圖片',
+        ),
+      ],
+    );
+    if (croppedFile == null) {
+      return; // 使用者取消
+    } else {
+      setState(() {
+        accountPhoto = File(croppedFile.path);
+      });
     }
   }
 }
